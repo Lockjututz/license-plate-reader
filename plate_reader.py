872921ec5dad26748145a6e9fb0841810f5ecb22
Matts Häggblom <matts.haggblom@gmail.com>
2022-03-05 15:35:08 +0200

    Add comments so it's actually understandable

--- plate_reader.py
+++ plate_reader.py
@@ -25,8 +25,10 @@ python plate_reader.py
 
 pytesseract.pytesseract.tesseract_cmd = "/usr/local/bin/tesseract"
 
+# Read the picture with the license plate
 img = cv2.imread("resources/tesla2.jpg")
 
+# Do some image enhancement
 resize_test_license_plate = cv2.resize(
     img, None, fx=2, fy=2,
     interpolation=cv2.INTER_CUBIC)
@@ -40,15 +42,18 @@ gaussian_blur_license_plate = cv2.GaussianBlur(
 img_rgb = cv2.cvtColor(gaussian_blur_license_plate, cv2.COLOR_BGR2RGB)
 
 # Plot and show the image (visible in the SciView Plot tab in PyCharm)
+# This isn't necessary, just fun to look at the picture after enhancement
 plt.imshow(img_rgb)
 plt.axis('on')
 plt.title('License plate')
 plt.show()
 
+# Actually parse the license plate number from the picture
 text = pytesseract.image_to_string(img_rgb,
                                    lang='swe',
                                    config='--oem 3 --psm 6')
 
+# Do a regexp search to find strings that match an actual license plate
 m = re.search('([A-Å]{2,} [0-9]{1,})', text)
 if m:
     license_plate_number = m.group(1)
